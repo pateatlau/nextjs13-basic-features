9848fb558a4ea7adc799c2b545d91196b29cc5fd
Patea Tlau <pateatlau@gmail.com>
2025-02-27 19:31:19 +0530

    First commit

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,19 +1,20 @@
 import Link from 'next/link';
-export default function Home() {
+
+export default function NewUser() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-2">
-      <div className="text-lg font-bold mb-2">Welcome to NextJs 13</div>
+      <div className="text-lg font-bold mb-2">New User Page</div>
       <Link
         className="text-blue-500 mb-2"
-        href="/users"
+        href="/"
       >
-        Users
+        Home
       </Link>
       <Link
         className="text-blue-500"
-        href="/users/new"
+        href="/users"
       >
-        New User
+        Users
       </Link>
     </main>
   );
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
-export default function Home() {
+
+export default function Users() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-2">
-      <div className="text-lg font-bold mb-2">Welcome to NextJs 13</div>
+      <div className="text-lg font-bold mb-2">Users Page</div>
       <Link
         className="text-blue-500 mb-2"
-        href="/users"
+        href="/"
       >
-        Users
+        Home
       </Link>
       <Link
         className="text-blue-500"
